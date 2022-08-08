d9bef65056b56da553fd8d8f68dff463b23d0d2d
NetDef <netdef@pop-os.localdomain>
2022-08-08 16:14:07 +0300

    my second Commit adding image filter code to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { Router, Request, Response } from 'express';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { request } from 'http';
 
 (async () => {
 
@@ -9,7 +11,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -29,18 +31,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+    if (!image_url) {
+      res.status(400).send('Image url is not valid');
+    }
+    const image_filtered = await filterImageFromURL(image_url);
+    res.status(200).sendFile(image_filtered, () => {
+      deleteLocalFiles([image_filtered]);
+    });
+  })
+
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
